426b3ab4b035ee25cb645ee8fd8d24f56d0cd1c8
rimuri03 <rizkimr@student.telkomuniversity.ac.id>
2023-03-26 22:12:45 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302210097/Program.cs
+++ tpmodul6_1302210097/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video Details");
+        Console.WriteLine("ID: " + this.id);
+        Console.WriteLine("Title: " + this.title);
+        Console.WriteLine("Play Count: " + this.playCount);
+    }
+}
+
+public class MainClass
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [Rizki Muhammad Ridwan]");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
